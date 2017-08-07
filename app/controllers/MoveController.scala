9f4b135eb9ddd13ddaa7e0e924fa61cbbf418ac6
Ramin <ramin.esfan@gmail.com>
2017-08-07 09:10:34 +0100

    making the rock

--- app/controllers/MoveController.scala
+++ app/controllers/MoveController.scala
@@ -4,6 +4,7 @@ import play.api.libs.json.Json
 import play.api.mvc.{Action, Controller}
 
 class MoveController extends Controller {
+
   def move() = Action {
     Ok(Json.toJson("ROCK"))
   }
--- app/controllers/StartController.scala
+++ app/controllers/StartController.scala
@@ -3,7 +3,9 @@ package controllers
 import play.api.mvc.{Action, Controller}
 
 class StartController extends Controller {
+
   def start() = Action {
     Ok
   }
+
 }
